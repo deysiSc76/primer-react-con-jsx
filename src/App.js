7d99050c1745297fbe7757f69022fd31a9ec3ad7
deysiSc76 <deysi.santacruz76@gmail.com>
2024-05-12 23:32:55 -0400

    PrimerReactConJsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola Dojo!</h1>
+      <div>
+      <h2>Cosas que tengo que hacer</h2>
+      <div>
+      <ul>
+        <li>Estudiar</li>
+        <li>Comer</li>
+        <li>Dormir</li>
+        <li>Estudiar</li>
+      </ul>
+      </div>
+      
+      </div>
+      
     </div>
   );
 }
